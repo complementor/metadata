042f2ab167df7ebc83a1edb1bcdf61fbf0125180
andreasbaggesgaard <andreas_baggesgaard@hotmail.com>
2020-01-28 15:16:39 +0100

    schema structure only

--- Datamodel/structuredump/ontology_metadata.sql
+++ Datamodel/structuredump/ontology_metadata.sql
@@ -16,13 +16,18 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `ontology`
+-- Table structure for table `metadata`
 --
 
-LOCK TABLES `ontology` WRITE;
-/*!40000 ALTER TABLE `ontology` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ontology` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `metadata` (
+  `Key` varchar(255) NOT NULL,
+  `Value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -33,4 +38,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-28 15:06:36
+-- Dump completed on 2020-01-28 15:14:55
--- Datamodel/structuredump/ontology_ontology.sql
+++ Datamodel/structuredump/ontology_ontology.sql
@@ -16,13 +16,22 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `metadata`
+-- Table structure for table `ontology`
 --
 
-LOCK TABLES `metadata` WRITE;
-/*!40000 ALTER TABLE `metadata` DISABLE KEYS */;
-/*!40000 ALTER TABLE `metadata` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `ontology`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ontology` (
+  `Id` int NOT NULL,
+  `Entity` varchar(255) DEFAULT NULL,
+  `Relation` varchar(255) DEFAULT NULL,
+  `EntityId` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `EntityId_idx` (`EntityId`),
+  CONSTRAINT `EntityId` FOREIGN KEY (`EntityId`) REFERENCES `ontology` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -33,4 +42,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-28 15:06:37
+-- Dump completed on 2020-01-28 15:14:55
