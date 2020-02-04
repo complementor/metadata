7c4954cebda90e83931f0c56fff2fb860aae48a5
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-02-04 21:29:57 +0100

    new scrapping

--- Scripts/youtubeExtraction/DownloadAllVidesFromUrlList.ps1
+++ Scripts/youtubeExtraction/DownloadAllVidesFromUrlList.ps1
@@ -1,7 +1,7 @@
+$source = "P:\resources\1000links.txt"
 $destination = "p:\resources\youtube\"
-$file = "p:\resources\youtube\1000links.txt"
 
-ForEach ($url in Get-Content $file) {
+ForEach ($url in Get-Content $source) {
     if ($url -match $regex) {
         $guid = [guid]::NewGuid().ToString();
         & "C:\Programming\FFmpeg\bin\youtube-dl.exe" --add-metadata $Url -o $destination$guid
