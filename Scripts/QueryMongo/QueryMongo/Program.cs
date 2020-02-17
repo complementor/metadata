64e36d330ecbe8f93a08a66dde8b039366745f6d
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-02-18 00:09:37 +0100

    initial code for model extensibility evaluation

---
+++ Scripts/QueryMongo/QueryMongo/Program.cs
@@ -0,0 +1,72 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+
+namespace QueryMongo
+{
+    internal static class Program
+    {
+        static IMongoDatabase database;
+        static void Main(string[] args)
+        {
+            var mongoCollection = GetMongoDbCollection("test");
+
+            ExtendDocument(mongoCollection, Samples.HubWithScenes, Samples.With1AlgmDoc, "optical_character_recognition");
+            ExtendDocument(mongoCollection, Samples.With1AlgmDoc, Samples.With2AlgmDoc, "speech_recognition");
+            ExtendDocument(mongoCollection, Samples.With2AlgmDoc, Samples.With3AlgmDoc, "sentiment_analysis");
+            ExtendDocument(mongoCollection, Samples.With3AlgmDoc, Samples.With4AlgmDoc, "objects");
+        }
+
+        private static void ExtendDocument(IMongoCollection<BsonDocument> mongoCollection, BsonDocument initialDocument,
+            BsonDocument extendedDocument, string indexName)
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            IMongoCollection<BsonDocument> collection = InitializeDatabase(initialDocument, mongoCollection);
+
+            Func<FilterDefinition<BsonDocument>> filter = () => builder.Eq("hub.satellite.video_information.scenes", 50);
+
+            Console.WriteLine(@"Apply the query:  SELECT * FROM document");
+            var initialHub = collection.Find(filter.Invoke()).First().AsBsonDocument;
+            Console.WriteLine($"Initial document : {initialHub}");
+
+            collection.ReplaceOne(filter.Invoke(), extendedDocument);
+            Console.WriteLine($" ");
+            Console.WriteLine($"Document is extended with: {indexName}");
+            Console.WriteLine($" ");
+
+            Console.WriteLine(@"Apply the query: SELECT * FROM document");
+            var documentAfterUpdate = collection.Find(filter.Invoke()).First().AsBsonDocument;
+            Console.WriteLine($"Extended document: {documentAfterUpdate}");
+
+            Console.ReadLine();
+        }
+
+        private static IMongoCollection<BsonDocument> GetMongoDbCollection(string collectionName)
+        {
+            Console.WriteLine("Connect to localhost");
+            const string connectionStirng = "mongodb://127.0.0.1:27017/?compressors=zlib&readPreference=primary&gssapiServiceName=mongodb&appname=MongoDB%20Compass%20Community&ssl=false";
+            MongoClient client = new MongoClient(connectionStirng);
+
+            Console.WriteLine($"Connect to database");
+            database = client.GetDatabase("metadata");
+
+            Console.WriteLine($"Connect to collection: {collectionName}");
+            return database.GetCollection<BsonDocument>(collectionName);
+        }
+
+        private static IMongoCollection<BsonDocument> InitializeDatabase(BsonDocument hubWithScenes, IMongoCollection<BsonDocument> collection)
+        {
+            var filter = Builders<BsonDocument>.Filter.Exists("hub");
+            var result = collection.Find(filter).ToList();
+            if (result.Count > 0)
+            {
+                database.DropCollection(collection.CollectionNamespace.CollectionName);
+            }
+
+            collection.InsertOne(hubWithScenes);
+            Console.WriteLine($" ");
+            return collection;
+        }
+    }
+}
---
+++ Scripts/QueryMongo/QueryMongo/SmapleGenerator.cs
@@ -0,0 +1,245 @@
+﻿using MongoDB.Bson;
+
+namespace QueryMongo
+{
+    public static class Samples
+    {
+        public static BsonDocument HubWithScenes
+        {
+            get => new BsonDocument
+                {
+                    { "hub", new BsonDocument
+                    {
+                        { "date", "14-02-2020 13:50:23" } ,
+                        { "satellite", new BsonDocument
+                        {
+                            { "video_information", new BsonDocument
+                            {
+                                { "scenes", 50 },
+                                { "frames", 5059},
+                                { "duration", "00:03:31.002"},
+                                { "subtitles", new BsonArray {}
+                }
+            }}}}}}};
+        }
+        public static BsonDocument With1AlgmDoc
+        {
+            get => new BsonDocument
+                {
+                    { "hub", new BsonDocument
+                    {
+                        { "date", "14-02-2020 13:50:23" } ,
+                        { "satellite", new BsonDocument
+                        {
+                            { "video_information", new BsonDocument
+                            {
+                                { "scenes", 50 },
+                                { "frames", 5059},
+                                { "duration", "00:03:31.002"},
+                                { "subtitles", new BsonArray {}}
+                            }
+                            },
+                            { "optical_character_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                        { "scene", 1},
+                                        { "start", "00:00:00.834"},
+                                        { "end", "00:00:14.348"},
+                                        { "frameStart", 20},
+                                        { "frameEnd", 344},
+                                        { "value", "Some OCR generated text"},
+                                    }
+                            }
+                            }
+            }}}}};
+        }
+        public static BsonDocument With2AlgmDoc
+        {
+            get => new BsonDocument
+                {
+                    { "hub", new BsonDocument
+                    {
+                        { "date", "14-02-2020 13:50:23" } ,
+                        { "satellite", new BsonDocument
+                        {
+                            { "video_information", new BsonDocument
+                            {
+                                { "scenes", 50 },
+                                { "frames", 5059},
+                                { "duration", "00:03:31.002"},
+                                { "subtitles", new BsonArray {}
+                            }}},
+                            { "optical_character_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                        { "scene", 1},
+                                        { "start", "00:00:00.834"},
+                                        { "end", "00:00:14.348"},
+                                        { "frameStart", 20},
+                                        { "frameEnd", 344},
+                                        { "value", ""},
+                                    },
+                                }
+                            },
+                            { "speech_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", "you better than other video with the girls get something on YouTube but I think I'm taking it might be incredibly useful I still don't really believe it when I saw this online and I said a demonstration as a couple"},
+                                },
+                            }
+                            }}}
+            }}};
+
+        }
+        public static BsonDocument With3AlgmDoc
+        {
+            get => new BsonDocument
+                {
+                    { "hub", new BsonDocument
+                    {
+                        { "date", "14-02-2020 13:50:23" } ,
+                        { "satellite", new BsonDocument
+                        {
+                            { "video_information", new BsonDocument
+                            {
+                                { "scenes", 50 },
+                                { "frames", 5059},
+                                { "duration", "00:03:31.002"},
+                                { "subtitles", new BsonArray {}
+},
+                            { "collaborative_metadata", new BsonArray {} },
+                            { "optical_character_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                        { "scene", 1},
+                                        { "start", "00:00:00.834"},
+                                        { "end", "00:00:14.348"},
+                                        { "frameStart", 20},
+                                        { "frameEnd", 344},
+                                        { "value", ""},
+                                    },
+                                }
+                            },
+                            { "speech_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", "you better than other video with the girls get something on YouTube but I think I'm taking it might be incredibly useful I still don't really believe it when I saw this online and I said a demonstration as a couple"},
+                                },
+                            }
+                            },
+                            { "sentiment_analysis",  new BsonArray
+                            {
+                                new BsonDocument
+                                {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", new BsonDocument
+                                        {
+                                            { "neg", 0.0 },
+                                            { "neu", 0.803 },
+                                            { "pos", 0.197 },
+                                            { "compound", 0.7808 },
+                                        }
+                                    }
+                                }
+                            }
+                            }}}}}}}};
+        }
+        public static BsonDocument With4AlgmDoc
+        {
+            get => new BsonDocument
+                {
+                    { "hub", new BsonDocument
+                    {
+                        { "date", "14-02-2020 13:50:23" } ,
+                        { "satellite", new BsonDocument
+                        {
+                            { "video_information", new BsonDocument
+                            {
+                                { "scenes", 50 },
+                                { "frames", 5059},
+                                { "duration", "00:03:31.002"},
+                                { "subtitles", new BsonArray {}
+},
+                            { "collaborative_metadata", new BsonArray {} },
+                            { "optical_character_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                        { "scene", 1},
+                                        { "start", "00:00:00.834"},
+                                        { "end", "00:00:14.348"},
+                                        { "frameStart", 20},
+                                        { "frameEnd", 344},
+                                        { "value", ""},
+                                    },
+                                }
+                            },
+                            { "speech_recognition", new BsonArray
+                            {
+                                new BsonDocument {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", "you better than other video with the girls get something on YouTube but I think I'm taking it might be incredibly useful I still don't really believe it when I saw this online and I said a demonstration as a couple"},
+                                },
+                            }
+                            },
+                            { "sentiment_analysis",  new BsonArray
+                            {
+                                new BsonDocument
+                                {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", new BsonDocument
+                                        {
+                                            { "neg", 0.0 },
+                                            { "neu", 0.803 },
+                                            { "pos", 0.197 },
+                                            { "compound", 0.7808 },
+                                        }
+                                    }
+                                }
+                            }
+                            },
+                            { "objects", new BsonArray
+                            {
+                                new BsonDocument
+                                {
+                                    { "scene", 1},
+                                    { "start", "00:00:00.834"},
+                                    { "end", "00:00:14.348"},
+                                    { "frameStart", 20},
+                                    { "frameEnd", 344},
+                                    { "value", new BsonArray
+                                    {
+                                        new BsonDocument
+                                        {
+                                            { "label", "person"},
+                                            { "confidence", 0.9997119307518005},
+                                        },
+                                        new BsonDocument
+                                        {
+                                            { "label", "dining table"},
+                                            { "confidence", 0.5953575372695923 },
+            }}}}}}}}}}}}};
+        }
+    }
+}
