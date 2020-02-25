c00257620c0cf3745b45673fcf215b4f3ee79346
Andrei <pirvandrei@protonmail.com>
2020-02-25 14:05:42 +0100

    added dateTime to comment

--- Tools/WebApp/WebApplication/MongoDbAccessLayer/Dtos/Comment.cs
+++ Tools/WebApp/WebApplication/MongoDbAccessLayer/Dtos/Comment.cs
@@ -1,10 +1,12 @@
-﻿namespace MongoDbAccessLayer.Dtos
+﻿using System;
+
+namespace MongoDbAccessLayer.Dtos
 {
     public class Comment
     {
         public string User { get; }
         public string UserIcon { get;}
         public string Description { get; }
-         
+        public DateTime DateTime { get; }
     }
 }
